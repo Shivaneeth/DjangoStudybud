ae2292e1eef36f82fea0f5ec4dd1d4473cbb8678
Shivaneeth <shivaneethp.1si17cs102@gmail.com>
2022-05-01 23:55:54 +0530

    added room_form.html to take input from user, implemented CURD operations

--- base/models.py
+++ base/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 
@@ -12,13 +12,17 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)  # save every time
     created = models.DateTimeField(auto_now_add=True)  # save only when created
     # participants
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        # ordering based on field, '-' repesents reverse order
+        ordering = ['-updated', '-created']
 
     def __str__(self) -> str:
         return self.name
--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
 
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/', views.mainRoom, name='mainRoom'),
     path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/', view=views.createRoom, name='create-room'),
+    path('update-room/<str:pk>/', view=views.updateRoom, name='update-room'),
+    path('delete-room/<str:pk>/', view=views.deleteRoom, name='delete-room'),
 ]
