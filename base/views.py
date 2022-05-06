b74035c47f32774ee5c324c3101eb999c48edaad
Shivaneeth <shivaneethp.1si17cs102@gmail.com>
2022-05-06 18:47:58 +0530

    added user management i.e login and logout and (login_register.html)

--- base/views.py
+++ base/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.db.models import Q
 from django.shortcuts import redirect, render
 
@@ -11,6 +14,33 @@ from .models import Room, Topic
 #     {'id': 2, 'name': 'Css'},
 #     {'id': 3, 'name': 'Html'},
 # ]
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exit')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
